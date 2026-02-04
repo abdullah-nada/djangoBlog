54bfa0029e5ce199d0219c2a74da823e772950ea
abdullah <anwerabdullah794@gmail.com>
2026-02-04 17:00:41 +0200

    adimn customization

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-
 from .models import Post
+
 # Register your models here.
 
-admin.site.register(Post)
+
+
+class Product_Admin(admin.ModelAdmin):
+    list_display=['title','content','draft']
+    list_filter=['draft']
+    search_fields=['title']
+
+
+
+admin.site.register(Post,Product_Admin)
--- posts/models.py
+++ posts/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 
 
-
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=1000)
     draft=models.BooleanField(default=True)
-    
 
 
+
+    def __str__(self):
+        return self.title
 
