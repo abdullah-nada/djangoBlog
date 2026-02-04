105b81d4aa6edec7c8f2c15e72cd292183bd9a9b
abdullah <anwerabdullah794@gmail.com>
2026-02-04 16:40:29 +0200

    design post model

---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=1000)
+    draft=models.BooleanField(default=True)
+    
+
+
+
